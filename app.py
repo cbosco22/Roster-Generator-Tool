fb90ee2a9ad302e39dae57ac5f4749dbf720c2fc
cbosco22 <bosco.chris01@gmail.com>
2026-06-10 01:28:55 -0400

    Fix HTML formatting in Streamlit app description

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ st.markdown(
     "<p style='color:#666;margin-top:0.2em'>Navy Baseball Recruiting Tools/p>"
 
 Streamlit app for coaches in the field:
-  * Generate pdf rosters, csv schedules, and post event summaries</p>",
+  * <Generate pdf rosters, csv schedules, and post event summaries/p>",
     unsafe_allow_html=True,
 )
 
